6b0e013e80282243c4afb99054cb18f64289ff97
Marc Pomar <marc@corecode.school>
2021-03-31 12:30:11 +0200

    changes

--- dist/utils/angle2rad.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var angle2rad = function (angle) { return (Math.PI / 180) * angle; };
-exports.default = angle2rad;
--- dist/utils/checkLimits.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var checkLimits = function (pos) {
-    if (pos.x < 600 && pos.x > -50 && pos.y < 400 && pos.y > -20) {
-        return true;
-    }
-    return false;
-};
-exports.default = checkLimits;
